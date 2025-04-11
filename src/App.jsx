41027eab93030d9a4991a261c0af8b841ac1b7da
MohammedHassan07 <mhassanch260@gmail.com>
2025-04-11 17:21:00 +0530

    scrollable height in LLM response

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-cyan-50 via-white to-blue-100 flex items-center justify-center px-4 py-10">
+    <div className="h-screen bg-gradient-to-r from-cyan-50 via-white to-blue-100 flex items-center justify-center p-4">
       <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="bg-white p-10 rounded-3xl shadow-xl border border-blue-100">
           <h2 className="text-2xl md:text-3xl font-extrabold tracking-wide text-teal-700 uppercase mb-8">
@@ -83,7 +83,7 @@ function App() {
           </button>
         </div>
 
-        <div className="bg-white p-10 rounded-3xl shadow-xl border border-blue-100">
+        <div className="bg-white p-10 rounded-3xl shadow-xl border border-blue-100 min-h-screen">
           <h2 className="text-2xl md:text-3xl font-extrabold tracking-wide text-blue-700 uppercase mb-6">
             🤖 LLM Response
           </h2>
@@ -92,14 +92,17 @@ function App() {
             <h3>Prediction: {data.prediction}</h3>
             <h3>Probability: {data.probability} %</h3>
           </div>
-          <div className="text-slate-700 text-lg min-h-[100px] leading-relaxed">
+          <div className="text-slate-700 text-lg leading-relaxed max-h-96 overflow-y-auto">
+
 
             {/* <p>{data.llmResponse}</p>
              */}
-            <div className="prose prose-lg max-w-none prose-headings:text-blue-700 prose-li:marker:text-teal-600 prose-strong:text-teal-700 prose-a:text-blue-500">
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <div className="  prose prose-lg max-w-none prose-headings:text-blue-700 prose-li:marker:text-teal-600 prose-strong:text-teal-700 prose-a:text-blue-500">
+              {/* <ReactMarkdown remarkPlugins={[remarkGfm]}>
                 {data.llmResponse}
-              </ReactMarkdown>
+              </ReactMarkdown> */}
+
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae culpa impedit repellendus velit ipsa? Et maxime eaque ipsum iste! Eum ipsa vel repudiandae et nobis asperiores libero fuga, velit saepe quos reprehenderit sunt accusantium unde culpa maiores harum aliquam obcaecati totam non? Obcaecati, sint incidunt, fugiat aliquid eum quod iure voluptas voluptatibus quisquam nihil dolore error molestias corporis perspiciatis quidem doloremque quibusdam. Repellendus accusantium illum, porro provident adipisci nobis molestiae labore illo neque possimus corporis omnis quos vel aspernatur consequuntur explicabo. Debitis quas nesciunt at, veritatis vitae non perferendis et, numquam voluptas veniam dignissimos laboriosam consequuntur possimus molestiae fugiat aperiam, velit amet fugit repellendus quam explicabo in iste? Velit distinctio eaque quia! Minima, aut dolor. Animi, et unde. Accusantium, iusto numquam? Molestiae accusantium quibusdam voluptatum, aut, nostrum error temporibus voluptate earum quisquam voluptatibus cumque molestias. Molestias, voluptate placeat laboriosam consequuntur labore cumque saepe consequatur fugiat magnam accusamus nihil rerum eligendi. Delectus esse eaque repellendus aliquam! Quibusdam at quis impedit ipsam iste beatae, repellat, temporibus illo nobis officiis, consectetur aliquam? Soluta ea nostrum repudiandae eligendi quibusdam temporibus ipsa, deserunt voluptates expedita deleniti placeat cumque, laborum cupiditate voluptate vel? Et, porro eum fugiat eos omnis aspernatur similique repellendus! A accusamus fuga iusto!
             </div>
 
 
